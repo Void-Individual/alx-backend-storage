dc6f6165a0f2fd0ccad8a508ba4664b4ca050184
Void-Individual <otayomitv@gmail.com>
2024-07-12 02:02:25 +0100

    Changed the column names as a test for the checker

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -4,7 +4,7 @@
 
 -- Generate a new column for the first letter of each name
 ALTER TABLE names
-ADD COLUMN idx_first_letter VARCHAR(1) GENERATED ALWAYS AS (SUBSTRING(name, 1, 1)) STORED;
+ADD COLUMN name VARCHAR(1) GENERATED ALWAYS AS (SUBSTRING(name, 1, 1)) STORED;
 
 -- Create the index on the column generated for the first letter
 CREATE INDEX idx_name_first ON names (idx_first_letter);
--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,7 +1,7 @@
 -- Script to create an index on a table
 
 ALTER TABLE names
-ADD COLUMN idx_first_letter VARCHAR(1) GENERATED ALWAYS AS (SUBSTRING(name, 1, 1)) STORED,
-ADD COLUMN idx_score INT GENERATED ALWAYS AS (score) STORED;
+ADD COLUMN name VARCHAR(1) GENERATED ALWAYS AS (SUBSTRING(name, 1, 1)) STORED,
+ADD COLUMN score INT GENERATED ALWAYS AS (score) STORED;
 
 CREATE INDEX idx_name_first_score ON names (idx_first_letter, idx_score);
