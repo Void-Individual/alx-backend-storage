e60973ae81b80820f9d503ec4586df351b5a7d73
Void-Individual <otayomitv@gmail.com>
2024-07-13 20:30:50 +0100

    modified the index files

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,10 +1,4 @@
 -- Script to create an index to a table
 
--- DROP INDEX idx_name_first ON names;
-
--- Generate a new column for the first letter of each name
-ALTER TABLE names
-ADD COLUMN name VARCHAR(1) GENERATED ALWAYS AS (SUBSTRING(name, 1, 1)) STORED;
-
--- Create the index on the column generated for the first letter
-CREATE INDEX idx_name_first ON names (idx_first_letter);
+-- Create the index on the first letter of every name on the column
+CREATE INDEX idx_name_first ON names (name(1));
--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,7 +1,3 @@
 -- Script to create an index on a table
 
-ALTER TABLE names
-ADD COLUMN name VARCHAR(1) GENERATED ALWAYS AS (SUBSTRING(name, 1, 1)) STORED,
-ADD COLUMN score INT GENERATED ALWAYS AS (score) STORED;
-
-CREATE INDEX idx_name_first_score ON names (idx_first_letter, idx_score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
