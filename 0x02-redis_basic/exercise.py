ccd55f92baf5e409988c20b0aec71e768ed0ff53
Void-Individual <otayomitv@gmail.com>
2024-07-17 22:20:54 +0100

    renamed the redis object

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,21 +13,21 @@ class Cache:
         """Init this instance of the class to create a new instance
         and flush the current database"""
 
-        self.__redis = redis.Redis(host='localhost', port=6379)
-        self.__redis.flushdb()
+        self._redis = redis.Redis(host='localhost', port=6379)
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method to generate a random key, store it in redis and
         return the key"""
 
         key = str(uuid.uuid4())
-        self.__redis.set(key, data)
+        self._redis.set(key, data)
         return key
 
     def get(self, key, fn: Callable) -> str:
         """Get method to retrieve the value of the key in desired format"""
 
-        value = self.__redis.get(key)
+        value = self._redis.get(key)
         if fn:
             return fn(value)
         return value
@@ -35,11 +35,11 @@ class Cache:
     def get_str(self, key) -> str:
         """Method to retrieve and return a str instead of binary"""
 
-        data = self.__redis.get(key)
+        data = self._redis.get(key)
         return str(data)
 
     def get_int(self, key) -> int:
         """Method to retrieve and return an int instead of binary"""
 
-        data = self.__redis.get(key)
+        data = self._redis.get(key)
         return int(data)
