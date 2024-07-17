12a62ef68a4a13d0426f421f6850558a178d0aac
Void-Individual <otayomitv@gmail.com>
2024-07-17 18:29:45 +0100

    Testing task 1 and 2

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Redis module"""
+
+import uuid
+import redis
+from typing import Any, Callable
+
+
+class Cache:
+    """A cache class to hold an instance of a redis client"""
+
+    def __init__(self) -> None:
+        """Init this instance of the class to create a new instance
+        and flush the current database"""
+
+        self.__redis = redis.Redis(host='localhost', port=6379)
+        self.__redis.flushdb()
+
+    def store(self, data: Any) -> str:
+        """Method to generate a random key, store it in redis and
+        return the key"""
+
+        key = str(uuid.uuid4())
+        self.__redis.set(key, data)
+        return key
+
+    def get(self, key, fn: Callable) -> str:
+        """Get method to retrieve the value of the key in desired format"""
+
+        value = self.__redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key) -> str:
+        """Method to retrieve and return a str instead of binary"""
+
+        data = self.__redis.get(key)
+        return str(data)
+
+    def get_int(self, key) -> int:
+        """Method to retrieve and return an int instead of binary"""
+
+        data = self.__redis.get(key)
+        return int(data)
